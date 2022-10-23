658bbcab7b53bdb7dbbfc5f97b5b27c8b29853bd
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-10-22 22:48:25 -0700

    Basic widget data validation

---
+++ src/utils/proxy/validate-widget-data.js
@@ -0,0 +1,19 @@
+import widgets from "widgets/widgets";
+
+export default function validateWidgetData(widget, endpoint, data) {
+    let valid = true;
+    let dataParsed;
+    try {
+        dataParsed = JSON.parse(data);
+    } catch (e) {
+        valid = false;
+    }
+
+    const validate = widgets[widget.type]?.mappings?.[endpoint]?.validate;
+    validate.forEach(key => {
+        if (dataParsed[key] === undefined) {
+            valid = false;
+        }
+    });
+    return valid;
+}
