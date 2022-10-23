c75b239ed5e27bdba625d84e9804ff5dfd0625e3
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-10-23 01:48:46 -0700

    Update error display styling

--- src/components/services/widget/error.jsx
+++ src/components/services/widget/error.jsx
@@ -1,4 +1,7 @@
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { IoAlertCircle } from "react-icons/io5";
+import classNames from "classnames";
 
 function displayError(error) {
   return JSON.stringify(error[1] ? error[1] : error, null, 4);
@@ -10,39 +13,51 @@ function displayData(data) {
 
 export default function Error({ error }) {
   const { t } = useTranslation();
+
+  const [open, setOpen] = useState(false);
+
+  function toggleOpen() {
+    setOpen(!open);
+  }
   
   if (error?.data?.error) {
     error = error.data.error; // eslint-disable-line no-param-reassign
   }
 
   return (
-    <div className="text-sm bg-rose-100 text-rose-900 dark:bg-rose-900 dark:text-rose-100 rounded-md p-2 m-1">
-      <div className="font-medium mb-1">Something went wrong.</div>
-      <details className="text-xs font-mono whitespace-pre-wrap break-all">
-        <summary>{t("widget.debug_info")}</summary>
-        <div className="bg-white p-2 text-rose-900">
-          <ul>
-            <li className="mb-2">
-              <span className="text-black">{t("widget.api_error")}:</span> {error.message}
-            </li>
-            {error.url && <li className="mb-2">
-              <span className="text-black">{t("widget.url")}:</span> {error.url}
-            </li>}
-            {error.rawError && <li className="mb-2">
-              <span className="text-black">{t("widget.raw_error")}:</span>
-              <div className="ml-2">
-                {displayError(error.rawError)}
-              </div>
-            </li>}
-            {error.data && <li className="mb-2">
-              <span className="text-black">{t("widget.response_data")}:</span>
-              <div className="ml-2">
-                {displayData(error.data)}
-              </div>
-            </li>}
-          </ul>
-        </div>
-      </details>
+    <div className="px-3 pb-3">
+      <button className={classNames(
+        "text-xs mb-0 p-1 rounded-[3px] bg-rose-900/90 text-theme-800 flex items-center border-rose-900 border",
+        open && "mb-2"
+       )} onClick={toggleOpen}>
+        <IoAlertCircle className="mr-1 w-4 h-4"/>{t("widget.api_error")} {t("widget.information")}
+      </button>
+      <div className={classNames(
+        "bg-white dark:bg-theme-200/30 rounded-[3px] transition-all overflow-hidden text-rose-900 text-xs font-mono whitespace-pre-wrap break-all",
+        open && "h-auto",
+        !open && "h-0"
+      )}>
+        <ul className="p-4">
+          <li className="mb-2">
+            <span className="text-black">{t("widget.api_error")}:</span> {error.message}
+          </li>
+          {error.url && <li className="mb-2">
+            <span className="text-black">{t("widget.url")}:</span> {error.url}
+          </li>}
+          {error.rawError && <li className="mb-2">
+            <span className="text-black">{t("widget.raw_error")}:</span>
+            <div className="ml-2">
+              {displayError(error.rawError)}
+            </div>
+          </li>}
+          {error.data && <li className="mb-2">
+            <span className="text-black">{t("widget.response_data")}:</span>
+            <div className="ml-2">
+              {displayData(error.data)}
+            </div>
+          </li>}
+        </ul>
+      </div>
     </div>
   );
 }
